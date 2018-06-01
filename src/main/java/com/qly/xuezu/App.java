88dc217da92d3917d84b9370873c5e81f4307ad2
齐立圆 <460570129@qq.com>
2018-06-01 12:23:18 +0800

    修改测试

--- src/main/java/com/qly/xuezu/App.java
+++ src/main/java/com/qly/xuezu/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class App {
 
 	public static void main(String[] args) {
+		//gai
 		System.out.println("Xuezu开始启动Spring boot");
 		SpringApplication.run(App.class, args);
 		System.out.println("Xuezu启动完毕");
