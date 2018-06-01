2180037a38b9cae45dd6ae35ded15380498d95e5
齐立圆 <460570129@qq.com>
2018-06-01 09:36:21 +0800

    初始架构

---
+++ src/main/java/com/qly/xuezu/App.java
@@ -0,0 +1,15 @@
+package com.qly.xuezu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Xuezu开始启动Spring boot");
+		SpringApplication.run(App.class, args);
+		System.out.println("Xuezu启动完毕");
+	}
+}
